0c73253cc0be0b0de45f4796bd0c0d143e7d24cb
mauriciocarvalho01 <developer.mauricio1@gmail.com>
2021-10-09 20:40:41 -0300

    Added routes to the app

--- src/api/app.js
+++ src/api/app.js
@@ -1,11 +1,17 @@
 const express = require('express');
-
+const routerRegister = require('./routes/users/register');
+const routerLogin = require('./routes/auth/login');
 const app = express();
+app.use(express.json());
+
+app.use(routerRegister)
+app.use(routerLogin)
 
 // Não remover esse end-point, ele é necessário para o avaliador
 app.get('/', (request, response) => {
   response.send();
 });
 // Não remover esse end-point, ele é necessário para o avaliador
+
 
 module.exports = app;

4ef26d822dfe288075a67f989eb9a04ae3f049ca
mauriciocarvalho01 <developer.mauricio1@gmail.com>
2021-10-13 14:16:38 -0300

    Created recipes auth

---
+++ src/api/routes/auth.js
@@ -0,0 +1,30 @@
+//Packages
+const express = require('express');
+const router = express.Router();
+
+//Controllers
+const authController = require('../controllers/authController');
+
+
+//Middlewares
+//Middleware responsável por retirar espaçoes em branco no cadastro. 
+const trim = require('../middlewares/trim');
+//Middleware responsável retornar os erros 
+const errors = require('../middlewares/errors');
+//Middleware responsável por verificar o json schema 
+const loginSchema = require('../middlewares/loginSchema');
+//Middleware responsável por verificar se o usuario existe
+const checkUser = require('../middlewares/checkUser');
+
+
+//2 - Crie um endpoint para o login de usuários
+router.post('/login',
+    trim,
+    loginSchema,
+    checkUser,
+    authController.login,
+    errors
+);
+
+
+module.exports = router;
